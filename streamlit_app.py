a615c2339bd00e5637a7d3c4d30b6f5bb66ce0e9
akashaw <47110715+akashaw@users.noreply.github.com>
2023-01-01 21:12:30 +0530

    Update streamlit_app.py
    added a header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.header('😊Breakfast Menu')
 streamlit.text('😂Omega 3 & Bluberry Oatmeal')
 streamlit.text('🤣Kale, Spinach & Rocket Smoothie')
 streamlit.text('❤️Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞🥖Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
